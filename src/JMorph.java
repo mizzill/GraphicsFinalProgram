2d0a4df4d2900275179e08ebfe66231447f0b37c
mizzill777 <mizzill777@gmail.com>
2017-11-14 13:44:11 -0500

    Added file menu with a few options. Gotta start somewhere

--- src/JMorph.java
+++ src/JMorph.java
@@ -8,10 +8,41 @@ public class JMorph extends JFrame {
     public JMorph(){
         super("JMorph brought to you by MC Productions");
 
+        setupMenu();
+
         setSize(800, 600 );
         setVisible(true);
     }
 
+    /*Helper Method
+    * Creates Menu bar and items, also adds handlers to them
+    * */
+    private void setupMenu(){
+        JMenu fileMenu = new JMenu("File");
+
+        //Change Source Image
+        JMenuItem changeSrcImage = new JMenuItem("Change Source Image");
+        fileMenu.add( changeSrcImage );
+        /*TODO Handler*/
+        //Change Destination Image
+        JMenuItem changeDestImage = new JMenuItem("Change Destination Image");
+        fileMenu.add( changeDestImage );
+        /*TODO Handler*/
+        //Save
+        JMenuItem saveCtrlPts = new JMenuItem("Save");
+        fileMenu.add( saveCtrlPts );
+        /*TODO Handler*/
+        //Reset
+        JMenuItem resetImgs = new JMenuItem("Reset");
+        fileMenu.add( resetImgs );
+        /*TODO Handler*/
+
+        //Add the menus to a JMenuBar
+        JMenuBar bar = new JMenuBar();
+        setJMenuBar( bar );
+        bar.add(fileMenu);
+
+    }
     //Initialize and run class
     public static void main(String args[])
     {
